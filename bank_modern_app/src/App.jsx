30b03f593e9b16e0e4b00b4982e416da1153ad2e
srijan-lamichhane <shreejanlamichhane14b2r@gmail.com>
2024-02-10 23:10:45 +0545

    complete setup

--- bank_modern_app/src/App.jsx
+++ bank_modern_app/src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import styles from './style'
+import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from './components'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => (
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  <div className='bg-primary w-full overflow-hidden'>
+    {/* primary color is defined in tailwind.config.js as 00040f(dark blue) */}
+    {/* overflow is generally used for scrolling. */}
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      {/* dynamically set the value of a class name based on some variable or condition */}
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.paddingX}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
+      </div>
+    </div>
+
+  </div>
+
+)
 
 export default App
---
+++ bank_modern_app/src/components/index.js
@@ -0,0 +1,25 @@
+import Navbar from './Navbar'
+import Billing from './Billing'
+import Hero from './Hero'
+import Stats from './Stats'
+import Business from './Business'
+import CardDeal from './CardDeal'
+import Testimonials from './Testimonials'
+import Clients from './Clients'
+import CTA from './CTA'
+import Footer from './Footer'
+
+
+
+export {
+    Navbar,
+    Hero,
+    Stats,
+    Business,
+    Billing,
+    CardDeal,
+    Testimonials,
+    Clients,
+    CTA,
+    Footer,
+};
