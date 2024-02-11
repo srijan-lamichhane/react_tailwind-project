0b5ef63f018299d69b17908e7f87c91c7ea417e6
srijan-lamichhane <shreejanlamichhane14b2r@gmail.com>
2024-02-11 22:45:01 +0545

    navbar starter

--- bank_modern_app/src/components/Navbar.jsx
+++ bank_modern_app/src/components/Navbar.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import {close, logo, menu} from '../assets'
+
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <nav>
+      <img src="{logo}" alt="WixBank" className='w-[124px] h-[32px]'/>
+      <ul>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#'>About Us</a></li>
+        <li><a href='#'>Features</a></li>
+        <li><a href='#'>Solution</a></li>
+      </ul>
+    </nav>
   )
 }
 
