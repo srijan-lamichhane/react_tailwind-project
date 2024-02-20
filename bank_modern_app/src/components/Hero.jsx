64d94169fe26782e97d4e7b61ed0907238001c6e
srijan-lamichhane <shreejanlamichhane14b2r@gmail.com>
2024-02-20 11:39:47 +0545

    discount setion in hero

--- bank_modern_app/src/components/Hero.jsx
+++ bank_modern_app/src/components/Hero.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
+import styles from '../style'
+import { discount } from '../assets'
 
 const Hero = () => (
 
-  <section>
+  <section id='home' className={`flex flex-col md:flex-row ${styles.paddingY}`}>
 
-    <div>
+    <div className={`text-white flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6 `}>
 
-      <div>
-        <img src="" alt="" />
-        <p><span> 20%</span> DISCOUNT FOR <span> 1 MONTH </span> ACCOUNT</p>
+{/* discount portion */}
+      <div className='flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2 '>
+        <img src={discount} alt="discount" className='w-[32px] h-[32px]' />
+        <p className={`${styles.paragraph} ml-2`}>
+          <span className='text-white'>20%</span> DISCOUNT FOR <span className='text-white'> 1 MONTH </span> ACCOUNT
+        </p>
       </div>
 
-      <div>
-        <h1>The Next Generation Payment Method.</h1>
+{/* heading portion */}
+      <div className='flex flex-row justify-between items-center w-full'>
+        <h1>The Next <br className='sm:block hidden'/> <span>Generation</span> Payment Method.</h1>
         <div>GetStarted</div>
       </div>
 
