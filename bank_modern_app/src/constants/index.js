e49089d98623abb13080fc36476b65f860831bc2
srijan-lamichhane <shreejanlamichhane14b2r@gmail.com>
2024-04-05 19:42:39 +0545

    feat: business section ( complete)

--- bank_modern_app/src/constants/index.js
+++ bank_modern_app/src/constants/index.js
@@ -37,4 +37,25 @@ export const stats = [
     title: 'TRANSACTION',
   }
 ];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "Rewards",
+    content: "The best credit cards offer some tantalizing combinations of promotions and prizes",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "100% Secured",
+    content: "We take proactive steps make sure your information and transactions are secure.",
+  },
+  {
+    id: "feature-1",
+    icon: send,
+    title: "Balance Transfer",
+    content: "A balance transfer credit card can save you a lot of money in interest charges.",
+  },
+];
   
