b72dd493da996cdc8472f524e2e3c26eb0404fa0
srijan-lamichhane <shreejanlamichhane14b2r@gmail.com>
2024-04-07 11:02:13 +0545

    feat: testimonials completed

--- bank_modern_app/src/components/Clients.jsx
+++ bank_modern_app/src/components/Clients.jsx
@@ -1,25 +1,7 @@
 import React from 'react'
-import styles from '../style'
-import FeedbackCard from './FeedbackCard'
 
 const Clients = () => (
-  <section id='clients' className={` ${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
-
-{/* to do (for gradient) */}
-    <div />
-
-    <div>
-      <h1>What people are <br /> saying about us</h1>
-      <div>
-        <p>Everything you need to accept card payments and grow your business anywhere on the planet.</p>
-      </div>
-    </div>
-
-    <div>
-      <FeedbackCard />
-    </div>
-
-  </section>
+<div>clients</div>
 )
 
 export default Clients
